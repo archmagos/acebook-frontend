eca2841c697837610c134d5af71d6eeb33cb098d
John Forster <j_forster@hotmail.co.uk>
2018-08-08 14:58:01 +0100

    API communication working

--- src/index.js
+++ src/index.js
@@ -3,14 +3,34 @@ import ReactDOM from 'react-dom';
 
 console.log('hello from index.js');
 
+function Post(props){
+  return (
+      <div className="post">
+        {props.value}
+      </div>
+  )
+}
+
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      post: 'aaa',
+    }
+  }
   render(){
     return(
-        <div>
-          <h1>Howdy from React</h1>
-        </div>
+      <div>{this.state.post}</div>
     )
   }
+
+  componentDidMount() {
+    console.log('API Called')
+    fetch('http://localhost:3000/posts/test')
+    .then(response => response.json())
+    .then(data => this.setState({ post: data.body }))
+  };
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
