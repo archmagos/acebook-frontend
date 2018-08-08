6444be9f3fb4e3d163b9a8ab8efabd20c93a7240
John Forster <j_forster@hotmail.co.uk>
2018-08-08 15:49:14 +0100

    Renders posts

--- src/index.js
+++ src/index.js
@@ -6,31 +6,45 @@ console.log('hello from index.js');
 function Post(props){
   return (
       <div className="post">
-        {props.value}
+        {props.message} posted at {props.time}
       </div>
   )
 }
 
-class App extends React.Component {
+class Timeline extends React.Component {
   constructor(){
     super();
     this.state = {
-      post: 'aaa',
+      posts: [],
     }
   }
   render(){
-    return(
-      <div>{this.state.post}</div>
-    )
+    const posts = this.state.posts;
+    let postsList = posts.map(function(post){
+      return <Post
+          message={post.message}
+          time={post.created_at}
+      />
+    });
+    return postsList
   }
 
   componentDidMount() {
     console.log('API Called')
-    fetch('http://localhost:3000/posts/test')
+    fetch('http://localhost:3000/posts')
     .then(response => response.json())
-    .then(data => this.setState({ post: data.body }))
-  };
+    .then(data =>
+        // console.log(data)
+        this.setState({ posts: data })
+    )
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  render(){
+     return(<Timeline />)
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
