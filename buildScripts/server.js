44e9a45e14d1aac137103e18b51a717d5959a9b9
JohnForster <j_forster@hotmail.co.uk>
2018-08-09 12:15:57 +0100

    Removed CORS config from server.js file.
    Unnecessary, as CORS is handled in rails API.

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -22,9 +22,3 @@ app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
