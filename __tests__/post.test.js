e6a850bffa79ced49886d8855505892e1bb9c227
Layth <laythq@gmail.com>
2018-08-09 13:57:37 +0100

    Enzyme integration

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import expect from 'expect';
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme';
 import {Post} from '../src/post.js';
+import * as enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter() });
 
 describe('Post', () => {
   it('Post renders the message given', () => {
-    const component = renderer.create(<Post message='Hello' time='Today' />);
-    const json = component.toJSON();
-    expect(json).toMatchSnapshot();
+    const component = shallow(<Post message='Hello' time='Today'/>);
+    expect(component).toMatchSnapshot();
   });
 });
+
+// const component = renderer.create(<Post message='Hello' time='Today' />);
