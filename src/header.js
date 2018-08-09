12f90b21da01b71b12ac9d504f921063bea7ac22
Fred <fredch87@outlook.com>
2018-08-09 11:06:19 +0100

    Basic header styling added

--- src/header.js
+++ src/header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import css from './header.css';
 
 export class Header extends React.Component {
   render(){
     return(
       <div id="header">
-        <p>Header content will go here</p>
+        <div id="header_title">
+          <p>Acebook</p>
+        </div>
+        <div id="header_links">
+          <p>View Posts | Sign Up | Log in</p>
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Header} from "./header.js";
 import {Timeline} from "./timeline";
+import css from './index.css';
 
 class App extends React.Component {
   render(){
     return(
       <div className="container">
-        <Header />
-        <Timeline />
+        <div className="container_header">
+          <Header />
+        </div>
+        <div className="container_content" id="content">
+          <Timeline />
+        </div>
       </div>
     )
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
