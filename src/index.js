cd029176287a5c451fbfb1a8db4ccb7ff34a1c69
Fred <fredch87@outlook.com>
2018-08-10 14:05:25 +0100

    Scrolling and footer work

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Header} from "./header.js";
 import {Timeline} from "./timeline";
-import {Footer} from "./footer";
 import css from './index.css';
 
 class App extends React.Component {
@@ -17,6 +16,17 @@ class App extends React.Component {
             <Timeline />
           </div>
         </div>
+      </div>
+    )
+  }
+}
+
+export class Footer extends React.Component {
+  render(){
+    return(
+      <div className="container_footer">
+        <p><b>Acebook</b> was a group project by <a href="https://github.com/Ben-893">Ben</a>, <a href="https://github.com/archmagos">Fred</a>, <a href="https://github.com/JohnForster">John</a>, <a href="https://github.com/laythq">Layth</a> & <a href="https://github.com/YvCodeHong">Yvonne</a>.</p>
+        <p>Created during week nine of the <b>Makers Academy</b> bootcamp in August 2018.</p>
       </div>
     )
   }
